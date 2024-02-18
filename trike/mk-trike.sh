4ebf1e443879e944f0001eed6afd62d85321f0d8
Nick-Tallguy <nick.allen.54@gmail.com>
2024-02-18 10:02:14 +0000

    adding and amending files to produce a working set that produces a basic trike map, but with too many barrier warnings, many of which are not on a likely trike route

--- trike/mk-trike.sh
+++ trike/mk-trike.sh
@@ -6,17 +6,16 @@ DESC="Garmin_compatible_trike_map"
 FAMILYNME=Tallguy-trike
 GMAKE=/home/nick/mapping/mkgmap
 NC_GMAKE=/home/nick/ncdata/mapping/Garmin
-POLY=${NC-MAKE}/mkgmap-resources/barriers.poly
-#POLY=${NC-MAKE}/mkgmap-resources/great-britain.poly
+POLY=${NC_GMAKE}/mkgmap-resources/barriers.poly
+#POLY=${NC_GMAKE}/mkgmap-resources/great-britain.poly
 PBF=/home/nick/mapping/mkgmap/pbf_downloads/barriers.osm.pbf
 #PBF=/home/nick/mapping/mkgmap/pbf_downloads/great-britain.osm.pbf
 MAPS=/home/nick/mapping/QMS/Maps
 TYPS=/home/nick/Github/mkgmap-styles-typ
-NC_STYLES=${TYPS}/mkgmap-styles-typ/${NME}
+NC_STYLES=${TYPS}/${NME}
 LOGFILE=/home/nick/logs/${NME}-${DATE}.log
 SCRIPTS=${TYPS}/gen-scripts
 ZIPPED=${GMAKE}/7-zipped
-DESTINATION=${NC_GMAKE}/hidden-scripts/send.sh	
 #
 ## Sorting the logging
 exec 3>&1 1>${LOGFILE} 2>&1
@@ -42,18 +41,15 @@ java -Xms1024m -Xmx7g  -jar ${NC_GMAKE}/mkgmap-progs/mkgmap-r4916/mkgmap.jar -c 
 echo "Creating the windows .exe file with makensis" $(date -u)
 makensis osmmap.nsi
 cd ${ZIPPED}
-echo "Zipping the windows file (needed for nextcloud)" $(date -u)
+echo "Zipping the windows file" $(date -u)
 7z a ${ZIPPED}/${NME}-winexe-${DATE} ${GMAKE}/work/${FAMILYNME}.exe
 echo "Creating torrent file" $(date -u)
 transmission-create ${NME}-winexe-${DATE}.torrent -c ${DESC} -t udp://tracker.opentrackr.org:1337/announce -t https://tracker2.ctix.cn:443/announce https://tracker1.520.jp:443/announce ${ZIPPED}/${NME}-winexe-${DATE}.7z
-echo "copying .exe files folder to destination" $(date -u)
-scp -r -P 22 ${ZIPPED}/${NME}-winexe-${DATE}* ${DESTINATION}
 # 
 echo "zipping gmapi files" $(date -u)
 7z a ${ZIPPED}/${NME}-gmapi-${DATE} ${GMAKE}/work/${FAMILYNME}.gmap
 echo "Creating gmapi torrent file" $(date -u)
 transmission-create ${NME}-gmapi-${DATE}.7z.torrent -c ${DESC} -t udp://tracker.opentrackr.org:1337/announce -t https://tracker2.ctix.cn:443/announce https://tracker1.520.jp:443/announce ${ZIPPED}/${NME}-gmapi-${DATE}.7z
-scp -r -P 22 ${ZIPPED}/${NME}-gmapi-${DATE}.* ${DESTINATION}
 #
 echo "moving gmapsupp to qmapshack map folder and renaming" $(date -u)
 mv ${GMAKE}/work/gmapsupp.img ${MAPS}/${NME}-${DATE}.img
@@ -66,14 +62,16 @@ cd ${ZIPPED}
 echo "Creating torrent file" $(date -u)
 transmission-create ${NME}-${DATE}.7z.torrent -c ${DESC} -t udp://tracker.opentrackr.org:1337/announce -t https://tracker2.ctix.cn:443/announce https://tracker1.520.jp:443/announce ${ZIPPED}/${NME}-${DATE}.7z
 #
-scp -P 22 ${ZIPPED}/${NME}-${DATE}.* ${DESTINATION}
+echo "copying 7-zipped folder contents to destination" $(date -u)
+cd ${NC_GMAKE}/hidden-scripts
+./send.sh
 #
-echo "sorting the file sync on destination" $(date -u)
-cd ${SCRIPTS}
-./mk-sync-needed.sh
+#echo "sorting the file sync on destination" $(date -u)
+#cd ${SCRIPTS}
+#./mk-sync-needed.sh
 ##
-echo "cleaning up - trashing files in 7-zipped folder and Maps folder" $(date -u)
-trash-put ${ZIPPED}/*
-trash-put ${MAPS}/*
-echo "Files transferred to destination & all finished - sync not requested" $(date -u)
+#echo "cleaning up - trashing files in 7-zipped folder and Maps folder" $(date -u)
+#trash-put ${ZIPPED}/*
+#trash-put ${MAPS}/*
+#echo "Files transferred to destination & all finished - sync not requested" $(date -u)
 
