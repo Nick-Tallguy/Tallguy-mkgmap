6e9959562116ed346db489664b626cef257a1110
Nick-Tallguy <nick.allen.54@gmail.com>
2024-10-31 20:35:20 +0000

    update scripts to account for 4gb rpi5 being tested

--- gen-scripts/space.sh
+++ gen-scripts/space.sh
@@ -2,12 +2,15 @@
 #
 ######   Deleting items from trash automatically.  
 #
-ALERT=35   # This figure in GB  IF SPACE AVAILABLE IS LOWER THAN 10gb, OUTPUT WILL NOT BE AN INTEGER! FILE PROTECTED CHMOD 555
+ALERT=35   # This figure in GB  IF SPACE AVAILABLE IS LOWER THAN 10gb, OUTPUT WILL NOT BE AN INTEGER! 
 #
 ### 	Sort out trash	###
 function SPC(){
 # below line is for mapping on lenovo running ubuntu server system 28/11/2023 (home is within /dev/sda2 - which is not filtered out with the grep -vE line.  
-df -h | grep -vE '^Filesystem|tmpfs|tmpfs|tmpfs|tmpfs' | awk '{ print $4 " " $1 }' | while read -r output;
+#df -h | grep -vE '^Filesystem|tmpfs|tmpfs|tmpfs|tmpfs' | awk '{ print $4 " " $1 }' | while read -r output;
+#
+# below line is for mappi = rpi5 running raspberrypi os.
+df -h | grep -vE '^Filesystem|udev|tmpfs|tmpfs|tmpfs|/dev/nvme0n1p1|tmpfs' | awk '{ print $4 " " $1 }' | while read -r output;
 do
   echo "$output"
   usep=$(echo "$output" | awk '{ print $1}' | cut -d'G' -f1 )
