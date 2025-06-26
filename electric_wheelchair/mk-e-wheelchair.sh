d4a17793ed22dcb773607c1211b8f2ebdb25e129
Nick-Tallguy <nick.allen.54@gmail.com>
2025-06-26 23:23:33 +0100

    Electric Wheelchair, #16 refers
    Stiles are now marked - all prevent access to electric wheelchair.

--- electric_wheelchair/mk-e-wheelchair.sh
+++ electric_wheelchair/mk-e-wheelchair.sh
@@ -24,33 +24,33 @@ trap "echo 'ERROR: An error occurred during execution, check log ${LOGFILE} for 
 trap '{ set +x; } 2>/dev/null; echo -n "[$(date -Is)] " set -x' DEBUG
 #
 ## Trash any existing o5m files
-trash-put ${PBF}/*.o5m
-echo "sorting the trash" $(date -u)
-cd ${SCRIPTS}
+#trash-put ${PBF}/*.o5m
+#echo "sorting the trash" $(date -u)
+#cd ${SCRIPTS}
 #./m93-space.sh
 #
 ## OSMCONVERT  
-osmconvert ${PBF}/${AREA}.osm.pbf --out-o5m >${PBF}/${AREA}.o5m
+#osmconvert ${PBF}/${AREA}.osm.pbf --out-o5m >${PBF}/${AREA}.o5m
 #
 ## OSMFILTER
 #
-osmfilter ${PBF}/${AREA}.o5m --parameter-file=${TYPS}/barriers_filtered/bar-parameters-1 -o=${PBF}/${AREA}-4.o5m     # bar-parameters-1 = highways except service, tracks and paths which are private or customers
-osmfilter ${PBF}/${AREA}.o5m --parameter-file=${TYPS}/barriers_filtered/bar-parameters-2 -o=${PBF}/${AREA}-5.o5m     # bar-parameters-2 = Private Customer access areas without barriers
+#osmfilter ${PBF}/${AREA}.o5m --parameter-file=${TYPS}/barriers_filtered/bar-parameters-1 -o=${PBF}/${AREA}-4.o5m     # bar-parameters-1 = highways except service, tracks and paths which are private or customers
+#osmfilter ${PBF}/${AREA}.o5m --parameter-file=${TYPS}/barriers_filtered/bar-parameters-2 -o=${PBF}/${AREA}-5.o5m     # bar-parameters-2 = Private Customer access areas without barriers
 #
-trash-put ${PBF}/${AREA}-?.osm.pbf
-osmium merge ${PBF}/${AREA}-4.o5m ${PBF}/${AREA}-5.o5m -o${PBF}/${AREA}-6.osm.pbf
+#trash-put ${PBF}/${AREA}-?.osm.pbf
+#osmium merge ${PBF}/${AREA}-4.o5m ${PBF}/${AREA}-5.o5m -o${PBF}/${AREA}-6.osm.pbf
 #
 cd ${GMAKE}/work
 #
 ## SPLITTER
-rm -r ${GMAKE}/splitter/*
-echo "starting splitter" $(date -u)
-java -Xmx14g -jar ${NC_GMAKE}/mkgmap-progs/splitter-r654/splitter.jar --output=pbf --output-dir=${GMAKE}/splitter --max-nodes=1400000 --mapid=10010001 --geonames-file=${NC_GMAKE}/mkgmap-resources/cities15000.zip   --polygon-file=${POLY} ${PBF}/${AREA}-6.osm.pbf
+#rm -r ${GMAKE}/splitter/*
+#echo "starting splitter" $(date -u)
+#java -Xmx14g -jar ${NC_GMAKE}/mkgmap-progs/splitter-r654/splitter.jar --output=pbf --output-dir=${GMAKE}/splitter --max-nodes=1400000 --mapid=10010001 --geonames-file=${NC_GMAKE}/mkgmap-resources/cities15000.zip   --polygon-file=${POLY} ${PBF}/${AREA}-6.osm.pbf
 #
 ### MKGMAP 
 echo "Starting mkgmap" $(date -u)
 rm -r ${GMAKE}/work/*
-java -Xms1024m -Xmx14g  -jar ${NC_GMAKE}/mkgmap-progs/mkgmap-r4923/mkgmap.jar -c ${NC_STYLES}/nick.args --family-name=${FAMILYNME} -c ${GMAKE}/splitter/template.args --description="Tallguy - barriers require attention" ${NC_STYLES}/5411.txt --gmapsupp  ##--gmapi --nsis
+java -Xms1024m -Xmx14g  -jar ${NC_GMAKE}/mkgmap-progs/mkgmap-r4923/mkgmap.jar -c ${NC_STYLES}/nick.args --family-name=${FAMILYNME} -c ${GMAKE}/splitter/template.args --description="Tallguy - electric wheelchair" ${NC_STYLES}/5411.txt --gmapsupp  ##--gmapi --nsis
 ##
 #echo "Creating the windows .exe file with makensis" $(date -u)
 #makensis osmmap.nsi
