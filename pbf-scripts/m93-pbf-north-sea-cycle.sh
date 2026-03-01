973705a6b6c2e728bba70f934bde219dc560d999
Nick-Tallguy <nick.allen.54@gmail.com>
2026-03-01 08:41:06 +0000

    pbf-north-sea-cycle - removed typo preventing update of map data

--- pbf-scripts/m93-pbf-north-sea-cycle.sh
+++ pbf-scripts/m93-pbf-north-sea-cycle.sh
@@ -27,7 +27,7 @@ trap "echo 'ERROR: An error occurred during execution, check log ${LOGFILE} for 
 trap '{ set +x; } 2>/dev/null; echo -n "[$(date -Is)] " set -x' DEBUG
 #
 echo "sorting the trash. Total time for file creation = 17 mins approx. 3277 MB max used" $(date -u)
-cd ${GHUB}/gen-scripts/gen-scripts
+cd ${GHUB}/gen-scripts
 ./m93-space.sh
 #
 cd ${PBF}
